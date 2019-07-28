1cd2939ff15f05037b574d44b0a4413a7dd6a502
Your Name <you@example.com>
2019-07-28 19:36:55 +0200

    Work in Progress

--- index.js
+++ index.js
@@ -1,86 +1,132 @@
 const mongo = require('mongodb');
 const client = new mongo.MongoClient('mongodb://localhost:27017', {useNewUrlParser: true});
 
-function markAsDone(collect, id){
-
-    // console.log('MARKaSdONE', collect);
-    collect.updateOne({ _id: mongo.ObjectID(id) },{ $set: { done: true } }
-    , err =>{
-        if(err){
-            console.log('blad z aktualizacją wynonanego zadania')
-        }else{
-            console.log('udalo się zrobić zadanie')
+function deleteTask(dbCollection, title) {
+    dbCollection.find({ title }).toArray((err, task) => {
+        if (err) {
+            console.log('problem listą: ', err);
+            client.close()
+        } else if (task.length !== 1) {
+            console.log('nie ma takiego zadaina');
+            client.close()
+        } else {
+            console.log('jest takie zadanie');
+            dbCollection.deleteOne(
+                { title },
+                { $set: { done: true } }, err => {
+                    if (err) {
+                        console.log('problem z usunięciem zadania: ', err)
+                    } else {
+                        console.log('zadanie zostało usunięte prawidłowo')
+                    }
+                    client.close()
+                })
         }
-        client.close();
     })
 }
 
-function showList(collect){
-    collect.find({}).toArray((err, todos) => {
-        if(err){
-            console.log('nie mogę pokazać listy', err);
-        } else{
-                const todosDone = todos.filter( todo => todo.done);
-                const todosToDo = todos.filter( todo => !todo.done);
-
-                console.log(`zadania zrobione w ilości: ${todosDone.length}`);
-            for( const todo of todosDone){
-                console.log(`   id: < ${todo._id} >  ${todo.title}`);
-            }
-            console.log(`zadania do zrobienia w ilości: ${todosToDo.length}`);
-            for (const todo of todosToDo) {
-                console.log(`   id: < ${todo._id} >  ${todo.title}`);
-            }
+function makeTaskDone(dbCollection, title){
+    dbCollection.find({title}).toArray((err, task) => {
+        if (err) {
+            console.log('problem listą: ', err);
+            client.close()
+        } else if (task.length !== 1) {
+           console.log('nie ma takiego zadaina');
+            client.close()
+        } else if (task[0].done){
+            console.log('to zadanie zostało już wcześniej zrobione');
+            client.close()
+        } 
+        else{
+            console.log('jest takie zadanie');
+            dbCollection.updateOne(
+                { title },
+                { $set: { done: true } }, err => {
+                    if (err) {
+                        console.log('problem z aktualizacją zadania: ', err)
+                    } else {
+                        console.log('zadanie zostało zaktualizowane prawidłowo')
+                    }
+                    client.close()
+                })
         }
     })
-    client.close();
 }
 
-function addNewTodo(todosCollection, title) {
-  console.log('hello from addNewTodo func: ', title);
-  todosCollection.insertOne({
-      title,
-      done: false,
-  }, err => {
-      if(err){
-          console.log('nie dodało się')
-      }else{
-          console.log('dodalo sie')
-      }
-      client.close();
-  })
+function addNewTask(dbCollection, title){
+    dbCollection.insertOne({
+        title,
+        done: false
+    }, err => {
+        if(err){
+            console.log('problem z dodaniem zadania: ', err)
+        } else {
+            console.log('zadanie zostało dodane prawidłowo')
+        }
+        client.close()
+    })
+   
+}
+
+function showAllList(dbCollection){
+    dbCollection.find({}).toArray((err, tasks) => {
+        if (err) {
+            console.log('problem listą: ', err);
+        } else {
+            console.log(`Patrz i podziwiaj:
+        ############\n`);
+            for(const task of tasks){
+                console.log(`id: < ${task._id} >
+                title: ${task.title}
+                done: ${task.done}
+                ***************************
+                `)
+            }
+            console.log('\n         $$$$$$$$$$$$')
+        }
+        client.close()
+    })
 }
 
 function doTheToDo(todosCollection){
-    const [commmand, ...args] = process.argv.splice(2);
-    console.log('command: ', commmand,'args:', args);
-
-    switch(commmand){
-        default:
-            console.log('give me a correct command!!!');
-            break;
+    const [command, ...args] = process.argv.splice(2);
+    console.log('polecenie: ', command, 'argument: ', args[0]);
+    switch(command) {
         case 'add':
-            addNewTodo(todosCollection, args[0]);
+            addNewTask(todosCollection, args[0]);
             break;
         case 'list':
-            showList(todosCollection);
+            showAllList(todosCollection);
             break;
         case 'done':
-            markAsDone(todosCollection, args[0]);
+            makeTaskDone(todosCollection, args[0]);
+            break;
+        case 'delete':
+            deleteTask(todosCollection, args[0]);
             break;
     }
 
-    client.close()
+  
 }
+
+
 client.connect(err =>{
     if(err){
-        console.log('blad polaczenia', err)
+        console.log('blad polaczenia', err);
     }else{
-        console.log('polaczono poprawnie')
+        console.log('polaczono poprawnie');
         const db = client.db('test');
         const todosCollection = db.collection('todos');
        
         doTheToDo(todosCollection);
         
+        
     }
 });
+
+
+
+//$$####################################################################
+
+
+
